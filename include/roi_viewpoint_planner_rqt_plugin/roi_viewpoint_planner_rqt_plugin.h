a0c4d04e7f58a8b30c501e4419f1126a564e22f4
Tobias Zaenker <tzaenker@informatik.uni-bonn.de>
2020-03-24 15:31:29 +0100

    Added plugin code and ui

---
+++ include/roi_viewpoint_planner_rqt_plugin/roi_viewpoint_planner_rqt_plugin.h
@@ -0,0 +1,37 @@
+#ifndef ROI_VIEWPOINT_PLANNER_RQT_PLUGIN_H
+#define ROI_VIEWPOINT_PLANNER_RQT_PLUGIN_H
+
+#include <QWidget>
+#include <ros/ros.h>
+#include <rqt_gui_cpp/plugin.h>
+#include "roi_viewpoint_planner_rqt_plugin/ui_roi_viewpoint_planner_rqt_plugin.h"
+
+namespace roi_viewpoint_planner_rqt_plugin
+{
+
+class RoiViewpointPlannerRqtPlugin : public rqt_gui_cpp::Plugin
+{
+  Q_OBJECT
+
+public:
+  RoiViewpointPlannerRqtPlugin();
+  virtual void initPlugin(qt_gui_cpp::PluginContext& context);
+  virtual void shutdownPlugin();
+  virtual void saveSettings(qt_gui_cpp::Settings& plugin_settings, qt_gui_cpp::Settings& instance_settings) const;
+  virtual void restoreSettings(const qt_gui_cpp::Settings& plugin_settings, const qt_gui_cpp::Settings& instance_settings);
+
+private slots:
+  void on_modeChangeBox_currentIndexChanged(int index);
+
+  void on_activateExecutionCheckBox_toggled(bool checked);
+
+private:
+  Ui::RoiViewpointPlannerRqtPlugin ui_;
+  QWidget* widget_;
+  ros::ServiceClient changePlannerModeClient;
+  ros::ServiceClient activatePlanExecutionClient;
+};
+
+}
+
+#endif // ROI_VIEWPOINT_PLANNER_RQT_PLUGIN_H
---
+++ src/roi_viewpoint_planner_rqt_plugin/roi_viewpoint_planner_rqt_plugin.cpp
@@ -0,0 +1,96 @@
+#include "roi_viewpoint_planner_rqt_plugin/roi_viewpoint_planner_rqt_plugin.h"
+
+#include <pluginlib/class_list_macros.h>
+#include <QStringList>
+#include <roi_viewpoint_planner/ChangePlannerMode.h>
+#include <std_srvs/SetBool.h>
+
+namespace roi_viewpoint_planner_rqt_plugin
+{
+
+RoiViewpointPlannerRqtPlugin::RoiViewpointPlannerRqtPlugin() :
+  rqt_gui_cpp::Plugin(),
+  widget_(0)
+{
+  setObjectName("RoiViewpointPlannerRqtPlugin");
+}
+
+void RoiViewpointPlannerRqtPlugin::initPlugin(qt_gui_cpp::PluginContext& context)
+{
+  // access standalone command line arguments
+  QStringList argv = context.argv();
+  // create QWidget
+  widget_ = new QWidget();
+  // extend the widget with all attributes and children from UI file
+  ui_.setupUi(widget_);
+  // add widget to the user interface
+  context.addWidget(widget_);
+
+  connect(ui_.modeChangeBox, SIGNAL(currentIndexChanged(int)), this, SLOT(on_modeChangeBox_currentIndexChanged(int)));
+  connect(ui_.activateExecutionCheckBox, SIGNAL(toggled(bool)), this, SLOT(on_activateExecutionCheckBox_toggled(bool)));
+
+  changePlannerModeClient = getNodeHandle().serviceClient<roi_viewpoint_planner::ChangePlannerMode>("/roi_viewpoint_planner/change_planner_mode");
+  activatePlanExecutionClient = getNodeHandle().serviceClient<std_srvs::SetBool>("/roi_viewpoint_planner/activate_plan_execution");
+}
+
+void RoiViewpointPlannerRqtPlugin::shutdownPlugin()
+{
+  // unregister all publishers here
+  changePlannerModeClient.shutdown();
+  activatePlanExecutionClient.shutdown();
+}
+
+void RoiViewpointPlannerRqtPlugin::saveSettings(qt_gui_cpp::Settings& plugin_settings, qt_gui_cpp::Settings& instance_settings) const
+{
+  // TODO save intrinsic configuration, usually using:
+  // instance_settings.setValue(k, v)
+}
+
+void RoiViewpointPlannerRqtPlugin::restoreSettings(const qt_gui_cpp::Settings& plugin_settings, const qt_gui_cpp::Settings& instance_settings)
+{
+  // TODO restore intrinsic configuration, usually using:
+  // v = instance_settings.value(k)
+}
+
+/*bool hasConfiguration() const
+{
+  return true;
+}
+
+void triggerConfiguration()
+{
+  // Usually used to open a dialog to offer the user a set of configuration
+}*/
+
+void RoiViewpointPlannerRqtPlugin::on_modeChangeBox_currentIndexChanged(int index)
+{
+  roi_viewpoint_planner::ChangePlannerMode changeMode;
+  changeMode.request.mode = index;
+  if (changePlannerModeClient.call(changeMode) && changeMode.response.success == true)
+  {
+    ui_.statusTextBox->setText("Mode change successful");
+  }
+  else
+  {
+    ui_.statusTextBox->setText("Mode change failed");
+  }
+}
+
+void RoiViewpointPlannerRqtPlugin::on_activateExecutionCheckBox_toggled(bool checked)
+{
+  std_srvs::SetBool activateExecution;
+  activateExecution.request.data = checked;
+  if (activatePlanExecutionClient.call(activateExecution) && activateExecution.response.success == true)
+  {
+    ui_.statusTextBox->setText("Activate execution successful");
+  }
+  else
+  {
+    ui_.statusTextBox->setText("Activate execution failed");
+  }
+}
+
+} // namespace roi_viewpoint_planner_rqt_plugin
+
+PLUGINLIB_EXPORT_CLASS(roi_viewpoint_planner_rqt_plugin::RoiViewpointPlannerRqtPlugin, rqt_gui_cpp::Plugin)
+//PLUGINLIB_DECLARE_CLASS(roi_viewpoint_planner_rqt_plugin, RoiViewpointPlannerRqtPlugin, roi_viewpoint_planner_rqt_plugin::RoiViewpointPlannerRqtPlugin, rqt_gui_cpp::Plugin)
